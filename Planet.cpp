80078914561b4bf19d00e9f9d51fcd85c73215f6
almar5 <157390902+almar5@users.noreply.github.com>
2024-01-29 16:15:13 +0000

    project commencmen

--- Planet.cpp
+++ Planet.cpp
@@ -12,5 +12,11 @@ int main()
     cout << "What celcius temp would you like to convert: ";
     cin >> celciusNum;
 
-    //convert to fahrenheit
+    //convert to fahrenheit is (celciusNum * (9/5.)) + 32
+    fahrenNum = (celciusNum * (9/5.)) + 32;
+
+    //output to console the new value
+    cout << celciusNum << " is " << fahrenNum << " in Fahrenheit." << endl;
+
+    return 0;
 }
