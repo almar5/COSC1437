836c502e78ef72ee3420f1123a9e89b41d3a549b
almar5 <157390902+almar5@users.noreply.github.com>
2024-01-29 16:02:23 +0000

    first commit

---
+++ Planet.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //declare variables
+    double celciusNum;
+    double fahrenNum;
+
+    //get double celcius number from user
+    cout << "What celcius temp would you like to convert: ";
+    cin >> celciusNum;
+
+    //convert to fahrenheit
+}
