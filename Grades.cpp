45e3f554e7d64acc1d0f7d7fcd9fba43182fedc7
almar5 <157390902+almar5@users.noreply.github.com>
2024-02-21 13:57:47 +0000

    committed

--- Grades.cpp
+++ Grades.cpp
@@ -13,18 +13,18 @@ int main()
     // Input data
     int numStudents;
 
-    // Prompt the user to enter the number of students (maximum 20)
+    //user enter the number of students (maximum 20)
     cout << "Enter the number of students (maximum 20): ";
     cin >> numStudents;
 
-    // Loop to get student information
+    // Loop to get student info
     for (int i = 0; i < numStudents; ++i) 
     {
         cout << "Enter information for student " << i + 1 << ":\n";
 
         // Get student name
         cout << "Name: ";
-        cin.ignore(); // To consume the newline character left in the buffer
+        cin.ignore(); 
         getline(cin, names[i]);
 
         // Get student age 
@@ -47,7 +47,7 @@ int main()
         while (!(grades[i] == 'A' || grades[i] == 'B' || grades[i] == 'C' || grades[i] == 'D' || grades[i] == 'F'));
     }
 
-    // Display student information
+    // Display 
     cout << "\nStudent Information:\n";
     for (int i = 0; i < numStudents; ++i) 
     {
